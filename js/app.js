5b7d2f829367ce972a2276122368b80fe72a59b4
Al Mohaimin Farabi <almohaiminfarabi36@gmail.com>
2021-09-02 23:06:42 +0600

    beautifiedd

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// get all the global useable component and element
 const inputedText = document.getElementById('input-field');
 const srarchBtn = document.getElementById('search-btn');
 const Found = document.getElementById('found');
@@ -5,17 +6,21 @@ Found.style.color= 'white'
 const searchResult = document.getElementById('search-result');
 const spinner = document.getElementById("spinner");
 
+// search btn click function
 srarchBtn.addEventListener('click', async() =>{
     const searchText = inputedText.value;
     searchResult.textContent = '';
     Found.innerHTML = 'Searching';
     Found.classList.remove('d-none');
+
+    // search field emty validation
     if(searchText ===''){
         searchResult.textContent = '';
         alert('Search field can not be emty')
         Found.innerHTML = "Please Search Something &#128533";
         Found.classList.remove('d-none');
     }
+    // if valid search 
     else{
         spinner.classList.remove('d-none');
         const url = `https://openlibrary.org/search.json?q=${searchText}`;
@@ -26,9 +31,12 @@ srarchBtn.addEventListener('click', async() =>{
     }
 });
 
+// display result in dom
 const displaySearchResult = (books) => {
     const usertSearchText = document.getElementById('input-field');
     const searchedWith = usertSearchText.value;
+
+    // if search result dont exist
     if(books.numFound === 0){
         inputedText.value = '';
         Found.innerHTML = `Sorry, Can't Find Anything For You with the word "${searchedWith}" &#128532`;
@@ -40,12 +48,12 @@ const displaySearchResult = (books) => {
         Found.innerHTML = `We Found <span class="text-success">${books.numFound}</span> Result For You Containing <span class="text-success"> ${searchedWith}</span>. Showing <span class="text-success">${books.docs.length}</span> Result Now`;
         inputedText.value = '';
         Found.classList.remove('d-none');
-        
+
             const book = books.docs;
             // console.log(book);
-           
             book.forEach(book =>{
-
+            
+            // tenary opretor to chnage undefine value to custom
             const title = book?.title?book.title:['not found'];
             const writer = book?.author_name?book.author_name:['not found'];
             const publisher = book?.publisher?book.publisher:['not found'];
@@ -55,7 +63,7 @@ const displaySearchResult = (books) => {
             const language = book?.language?book.language:['not found'];
             const subject = book?.subject?book.subject:['not found'];
             
-    
+            // book image function
             const images = () => {
                 let img;
                 if(book.hasOwnProperty('cover_i') === true){
@@ -68,6 +76,7 @@ const displaySearchResult = (books) => {
                 }
             }
 
+            // update the book component in dom
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
